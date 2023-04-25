ccb45dcf5043401e503a42daab06df160b4f4201
ST10090661 <126698741+ST10090661@users.noreply.github.com>
2023-04-25 07:40:03 +0000

    local rep created

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using APIDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace APIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet ]
+        [Route("Product")]
+        public string GetProduct()
+        {
+            return "Milk";
+        }
+
+        [HttpGet]
+        [Route("All")]
+        public IEnumerable<Product> GetProducts() {
+            return new List<Product>
+            {
+                new Product(1,"Coke"),
+                new Product(1,"Butter"),
+                new Product(1,"milk"),
+                new Product(1,"sweets"),
+            };
+}
+    }
+}
